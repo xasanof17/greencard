e2510d2594fc7d09709f10e1b62b77603f720d65
Javohir <xasanof17@gmail.com>
2023-10-06 15:26:35 +0300

    banner changes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -18,17 +18,20 @@ const Banner = ({ className }: BannerProps) => {
           alt=""
           fill
           className="rounded-xl object-cover"
+          priority
         />
-        <div className="absolute left-0 top-0 flex h-full flex-col items-start justify-center px-3 py-10 backdrop-blur-sm md:backdrop-blur-[8px] lg:justify-start">
-          <h1 className="leading-12 w-full text-xl font-bold tracking-wide text-white md:text-4xl lg:max-w-sm xl:text-white">
-            Greencard DV-2025 Registration Period
-          </h1>
-          <p className="my-4 text-base text-white md:my-6 md:text-lg xl:text-xl xl:text-white">
-            Greencards.uz sizni Amerika DV Lottery dasturida rasmiy ro&apos;
-            yxatdan o&apos;tkazadi
-          </p>
-          <div className="inline-block">
-            <Button>Anketani to&apos;ldirish</Button>
+        <div className="absolute left-0 top-0 flex h-full flex-col items-start justify-center backdrop-blur-sm lg:justify-start">
+          <div className="px-3 py-10">
+            <h1 className="leading-12 w-full text-xl font-bold tracking-wide text-white md:text-4xl lg:max-w-sm xl:text-white">
+              Greencard DV-2025 Registration Period
+            </h1>
+            <p className="my-4 text-base text-white md:my-6 md:text-lg xl:text-xl xl:text-white">
+              Greencards.uz sizni Amerika DV Lottery dasturida rasmiy ro&apos;
+              yxatdan o&apos;tkazadi
+            </p>
+            <div className="inline-block">
+              <Button>Anketani to&apos;ldirish</Button>
+            </div>
           </div>
         </div>
       </div>
