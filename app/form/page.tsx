d7ba9abcd08c4493e58d01d5097a5a1893e58b09
Javohir <xasanof17@gmail.com>
2023-10-06 15:22:31 +0300

    fix bugs

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -5,13 +5,12 @@ export default function FormPage() {
     <section id="formpage" role="main" className="container py-10">
       <Welcome />
       <p className="mb-4 text-center text-base font-bold text-black">
-        After reading the instructions, click the "Begin Entry" button below to
-        enter the DV Program.
+        After reading the instructions, click the &quot;Begin Entry&quot; button
+        below to enter the DV Program.
       </p>
       <Block
         title="Entry Instructions"
-        text="Please read the DV-2025 Entry Instructions carefully. Entrants may be disqualified for not completing the entry form correctly or by
-submitting more than one entry. Use the link below to view the instructions."
+        text="Please read the DV-2025 Entry Instructions carefully. Entrants may be disqualified for not completing the entry form correctly or by submitting more than one entry. Use the link below to view the instructions."
       />
       <Form />
     </section>
--- components/Features.tsx
+++ components/Features.tsx
@@ -5,7 +5,7 @@ const Features = () => {
     <section role="main" id="features" className="py-10 lg:py-20">
       <div className="container flex flex-col">
         <h2 role="heading" className="title mb-5 md:mb-9">
-          Biz bilan bo'lishning afzalliklari
+          Biz bilan bo&apos;lishning afzalliklari
         </h2>
         <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:gap-5">
           {features.map(({ icon, text, title }, i) => (
