3af0c0d6e2131a2011f897c8909330e2a708bef0
Javohir <xasanof17@gmail.com>
2023-10-06 21:08:56 +0300

    fix env error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -16,7 +16,7 @@ type LayoutProps = {
 };
 
 async function getMessages(locale: string) {
-  const url = `https://api.i18nexus.com/project_resources/translations/${locale}.json?api_key=${process.env.NEXT_PUBLIC_I18NEXUS_API_KEY}`;
+  const url = `https://api.i18nexus.com/project_resources/translations/${locale}.json?api_key=${process.env.I18NEXUS_API_KEY}`;
   try {
     const res = await fetch(url, {
       next: { revalidate: false },
