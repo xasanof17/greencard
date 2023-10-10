40e03ebd8194f7063e7fc2375e86b2547af175fa
Javohir <xasanof17@gmail.com>
2023-10-10 18:39:03 +0300

    fix service text bug

--- components/Service/Service.tsx
+++ components/Service/Service.tsx
@@ -8,13 +8,13 @@ const Service = () => {
     <section id="service_page" role="main" className="my-10">
       <div className="container flex flex-col space-y-16">
         <ul className="ml-3 flex list-disc flex-col space-y-3 text-base font-medium text-[#333]">
-          <li>DV 2024 Green Card dasturiga ro'yxatga olish.</li>
-          <li>Green Card g'oliblarga DS-260 formalarini to'ldirish DV 2023</li>
-          <li>G'oliblarni barcha hujjatlarini suhbatgacha tayyorlab berish.</li>
+          <li>DV 2024 Green Card dasturiga ro&apos;yxatga olish.</li>
+          <li>Green Card g&apos;oliblarga DS-260 formalarini to&apos;ldirish DV 2023</li>
+          <li>G&apos;oliblarni barcha hujjatlarini suhbatgacha tayyorlab berish.</li>
           <li>
-            Green Card g'oliblarini (TIC Tashkent International Clinic) Toshkent
+            Green Card g&apos;oliblarini (TIC Tashkent International Clinic) Toshkent
             Xalqaro Klinikasiga hujjatlar tayyorlab berish hamda navbatga
-            qo'yish.
+            qo&apos;yish.
           </li>
           <li>Hujjatlarni ingliz tiliga tarjima qilish.</li>
           <li>
@@ -25,11 +25,11 @@ const Service = () => {
         </ul>
 
         <ol className="ml-3 flex list-decimal flex-col space-y-3 text-base font-medium">
-          <h3>Kerakli hujjatlar ro'yxati:</h3>
+          <h3>Kerakli hujjatlar ro&apos;yxati:</h3>
           <li>
             Xalqaro yoki Yashil (qizil, загран biometik yashil) pasport nusxasi.
           </li>
-          <li>Tug'ilgan haqida guvohnomalardan nusxa.</li>
+          <li>Tug&apos;ilgan haqida guvohnomalardan nusxa.</li>
           <li>Diplom va attestatdan nusxa.</li>
           <li>
             Green Card uchun rasm 600x600 pixelda yoki 5x5 smda elektron nusxada
